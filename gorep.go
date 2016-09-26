069ea0128f62164ad3cc9652b76b394a45a506cb
novalagung <caknopal@gmail.com>
2016-09-26 21:12:32 +0700

    no message

--- gorep.go
+++ gorep.go
@@ -3,35 +3,48 @@ package main
 import "path/filepath"
 import "flag"
 import "fmt"
+
 import "strings"
 import "io/ioutil"
+import "regexp"
 import "os"
 
 func main() {
-	var flagPath string
+	var flagPath, flagFrom, flagTo string
 	flag.StringVar(&flagPath, "path", "", "path files to replace")
-	var flagFrom string
 	flag.StringVar(&flagFrom, "from", "", "strings to replace")
-	var flagTo string
 	flag.StringVar(&flagTo, "to", "", "strings to replace")
-
 	flag.Parse()
 
 	if flagFrom == "" {
+		fmt.Println("ERROR", `argument -from="" is required`)
 		return
-	} else {
-		flagFrom = fmt.Sprintf("%s%s", `"`, flagFrom)
 	}
 
-	if flagTo != "" {
-		flagTo = fmt.Sprintf("%s%s", `"`, flagTo)
+	if flagTo == "" {
+		fmt.Println("ERROR", `argument -to="" is required`)
+		return
 	}
 
 	if flagPath == "." || flagPath == "" {
 		flagPath, _ = os.Getwd()
 	}
 
-	err := filepath.Walk(flagPath, func(path string, info os.FileInfo, err error) error {
+	regexImport, err := regexp.Compile(`(?s)(import(.*?)\)|import.*$)`)
+	if err != nil {
+		fmt.Println("ERROR", err.Error())
+		return
+	}
+
+	regexImportedPackage, err := regexp.Compile(`"(.*?)"`)
+	if err != nil {
+		fmt.Println("ERROR", err.Error())
+		return
+	}
+
+	found := []string{}
+
+	err = filepath.Walk(flagPath, func(path string, info os.FileInfo, err error) error {
 		if filepath.Ext(path) == ".go" {
 			bts, err := ioutil.ReadFile(path)
 			if err != nil {
@@ -39,9 +52,30 @@ func main() {
 			}
 
 			content := string(bts)
-			if strings.Contains(content, flagFrom) {
-				content = strings.Replace(content, flagFrom, flagTo, -1)
-				fmt.Println(path)
+			matches := regexImport.FindAllString(content, -1)
+			isExists := false
+
+		isReplacable:
+			for _, each := range matches {
+				for _, eachLine := range strings.Split(each, "\n") {
+					matchesInline := regexImportedPackage.FindAllString(eachLine, -1)
+					if err != nil {
+						return err
+					}
+
+					for _, eachSubline := range matchesInline {
+						if strings.Contains(eachSubline, flagFrom) {
+							isExists = true
+							break isReplacable
+						}
+					}
+				}
+			}
+
+			if isExists {
+				content = strings.Replace(content, `"`+flagFrom+`"`, `"`+flagTo+`"`, -1)
+				content = strings.Replace(content, `"`+flagFrom+`/`, `"`+flagTo+`/`, -1)
+				found = append(found, path)
 			}
 
 			err = ioutil.WriteFile(path, []byte(content), info.Mode())
@@ -52,7 +86,19 @@ func main() {
 
 		return nil
 	})
+
 	if err != nil {
 		fmt.Println("ERROR", err.Error())
 	}
+
+	for _, path := range found {
+		fmt.Printf("found in %s\n", path)
+	}
+
+	if len(found) == 0 {
+		fmt.Println("Nothing replaced")
+	} else {
+		fmt.Printf("Total %d file replaced\n", len(found))
+	}
+
 }
